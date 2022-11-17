bda3fad193fd229f8b5f0524cccb588b8f5889c5
Antikar <64309010048@tnk.ac.th>
2022-11-17 09:21:51 +0700

    Create ex5.js

---
+++ Week5/ex5.js
@@ -0,0 +1,13 @@
+let items ={
+    "A4paper":120,
+    "Ruler":25,
+    "FlashDrive":130
+}
+//Mr. Jib
+console.log("Mr. Jib",items.A4paper*5 +items.Ruler*2)
+
+//Mr. Fah
+console.log("Mr. Fah",items.FlashDrive*2 +items.A4paper*3)
+
+//Ms. See
+console.log("Ms. See",items.FlashDrive*2 +items.A4paper*3+items.Ruler*2)
